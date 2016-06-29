5868cb4e23d0abb0855dbad7d70d549c7c1c68dd
Eric Mrak <mail@ericmrak.info>
2016-06-28 20:35:41 -0700

    tput smkx if supported by the terminal

--- conf.d/mrak.fish
+++ conf.d/mrak.fish
@@ -5,3 +5,15 @@ set Z_DATA $XDG_DATA_HOME/z/data
 complete --command g --wraps git
 
 eval (python -m virtualfish ^/dev/null)
+
+# if the terminal supports it, set keyboard_transmit mode
+# fixes st
+if tput smkx ^/dev/null
+    function mrak_enable_keypad_transmit --on-event fish_postexec
+        tput smkx ^/dev/null
+    end
+
+    function mrak_disable_keypad_transmit --on-event fish_preexec
+        tput rmkx ^/dev/null
+    end
+end
