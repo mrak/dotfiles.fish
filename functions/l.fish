483fa6d7b31f79df55d6e179aaad52ac14f4be71
Eric Mrak <mail@ericmrak.info>
2017-01-05 09:07:25 -0800

    fix l alias on macOS

--- functions/l.fish
+++ functions/l.fish
@@ -1,4 +1,14 @@
-function l --description "ls without hidden files"
-    ls --ignore=".*" $argv
+switch (uname)
+    case Linux
+        function l --description "ls without hidden files"
+            ls --ignore=".*" $argv
+        end
+    case Darwin
+        function l --description "ls without hidden files"
+            command ls -Fh $argv
+        end
+    case '*'
+        function ls
+            command ls $argv
+        end
 end
-
