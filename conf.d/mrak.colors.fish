06e5319e3c11cbdcf3b67844b284a6731f1e094b
Eric Mrak <mail@ericmrak.info>
2017-01-05 20:59:20 -0800

    default fish theme things added

--- conf.d/mrak.colors.fish
+++ conf.d/mrak.colors.fish
@@ -1,3 +1,23 @@
 set fish_color_user blue
 set fish_color_host magenta
 set fish_color_cwd green
+set fish_color_autosuggestion 555
+set fish_color_command 005fd7
+set fish_color_comment 990000
+set fish_color_cwd green
+set fish_color_cwd_root red
+set fish_color_end 009900
+set fish_color_error ff0000
+set fish_color_escape --bold
+set fish_color_history_current --bold
+set fish_color_host magenta
+set fish_color_match --background=brblue
+set fish_color_normal normal
+set fish_color_operator bryellow
+set fish_color_param 00afff
+set fish_color_quote 999900
+set fish_color_redirection 00afff
+set fish_color_search_match bryellow --background=brblack
+set fish_color_selection white --bold --background=brblack
+set fish_color_user blue
+set fish_color_valid_path --underline
