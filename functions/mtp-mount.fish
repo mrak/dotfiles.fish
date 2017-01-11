9fc9999b7e5218df9eb824c8e1b913444c48ab80
Eric Mrak <mail@ericmrak.info>
2017-01-10 19:31:58 -0800

    no more mtp

--- functions/mtp-mount.fish
+++
@@ -1,5 +0,0 @@
-function mtp-mount --description "mount device over mtp to ~/MTP"
-    mkdir $HOME/MTP
-    simple-mtpfs $HOME/MTP
-end
-
--- functions/mtp-umount.fish
+++
@@ -1,5 +0,0 @@
-function mtp-umount --description "unmount MTP device"
-    fusermount -u $HOME/MTP
-    rmdir $HOME/MTP
-end
-
