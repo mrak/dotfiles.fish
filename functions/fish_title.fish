cd2497c534c0488b77203ea76d99afe16868c8aa
Eric Mrak <mail@ericmrak.info>
2018-05-21 23:24:47 +0000

    fix local variable setting mistake

--- functions/fish_title.fish
+++ functions/fish_title.fish
@@ -1,7 +1,7 @@
 function fish_title --description "Sets the terminal title"
     set -l LOCATION (tty | string replace '/dev/' '')
     if set -q SSH_CLIENT
-        set -l LOCATION (hostname)
+        set LOCATION (hostname)
     end
     set -l DIR (string replace $HOME '~' $PWD)
     echo $LOCATION $DIR
