c0f68922a9613b6239f4345ba4bd7d2c7eb4138a
Eric Mrak <enmrak@gmail.com>
2020-04-16 22:40:27 -0700

    Don't use mimeo on linux anymore

--- functions/o.fish
+++ functions/o.fish
@@ -1,7 +1,7 @@
 switch (uname)
     case Linux
         function o --description 'open file with preferred program'
-            mimeo $argv
+            xdg-open $argv
         end
     case Darwin
         function o --description 'open file with preferred program'
