ed63c3334667ff953a64846271d2c67498388137
Eric Mrak <enmrak@gmail.com>
2022-03-23 22:02:04 -0700

    fix when no k8s config

--- functions/__mrak_prompt_k8s.fish
+++ functions/__mrak_prompt_k8s.fish
@@ -1,6 +1,7 @@
 function __mrak_prompt_k8s --description "prompt info for kubernetes context"
+    printf ' '
     command -s kubectl >/dev/null; or return
-    [ ! -f ~/.kube/config ]; and return
+    [ -f ~/.kube/config ]; or return
 
     set_color blue
     kubectl config get-contexts | awk '/\*/ {printf "⬟ %s %s ", $2, $5}'
