60280cf756b4381f1ee3e50282ab3002eac79aac
Eric Mrak <emrak@brex.com>
2023-05-10 17:30:19 -0700

    vi mode prompt

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,9 +1,23 @@
 function fish_right_prompt
-    if [ ! $__mrak_prompt_status = 0 ]
+  switch $fish_bind_mode
+    case default
+      set_color --bold red
+      echo 'NORMAL'
+    case insert
+      if [ ! $__mrak_prompt_status = 0 ]
         set_color brblack;
         printf '%s' $__mrak_prompt_status
-        set_color normal
-    end
-
-    set -e __mrak_prompt_status
+      end
+      set -e __mrak_prompt_status
+    case replace_one
+      set_color --bold green
+      echo 'REPLACE'
+    case visual
+      set_color --bold brmagenta
+      echo 'VISUAL'
+    case '*'
+      set_color --bold red
+      echo '?'
+  end
+  set_color normal
 end
--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -1,4 +1,7 @@
 function fish_user_key_bindings
+    fish_default_key_bindings -M insert
+    fish_vi_key_bindings --no-erase insert
+
     type -q fzf_key_bindings; and fzf_key_bindings
     bind \e. 'history-token-search-backward'
 
