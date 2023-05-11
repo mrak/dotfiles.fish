d870c92b5886889747662d53b691bc235fa9c4a1
Eric Mrak <emrak@brex.com>
2023-05-10 17:34:09 -0700

    restore right prompt

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,23 +1,9 @@
 function fish_right_prompt
-  switch $fish_bind_mode
-    case default
-      set_color --bold red
-      echo 'NORMAL'
-    case insert
-      if [ ! $__mrak_prompt_status = 0 ]
+    if [ ! $__mrak_prompt_status = 0 ]
         set_color brblack;
         printf '%s' $__mrak_prompt_status
-      end
-      set -e __mrak_prompt_status
-    case replace_one
-      set_color --bold green
-      echo 'REPLACE'
-    case visual
-      set_color --bold brmagenta
-      echo 'VISUAL'
-    case '*'
-      set_color --bold red
-      echo '?'
-  end
-  set_color normal
+        set_color normal
+    end
+
+    set -e __mrak_prompt_status
 end
