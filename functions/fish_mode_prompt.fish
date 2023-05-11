cace198852dd307c8d4acd5783e7308f6b5d0d11
Eric Mrak <emrak@brex.com>
2023-05-10 17:36:17 -0700

    mode prompt

--- functions/fish_mode_prompt.fish
+++ functions/fish_mode_prompt.fish
@@ -1,2 +1,16 @@
 function fish_mode_prompt
+  set_color brblack;
+  switch $fish_bind_mode
+    case default
+      echo 'N '
+    case insert
+      echo 'I '
+    case replace_one
+      echo 'R '
+    case visual
+      echo 'V '
+    case '*'
+      echo '? '
+  end
+  set_color normal
 end
