a6fd2d6e658376479f43a37ed76c47f736bbe245
Eric Mrak <enmrak@gmail.com>
2023-06-07 14:27:52 -0700

    don't reset the prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,15 +1,5 @@
 function fish_prompt
     set -g __mrak_prompt_status (fish_status_to_signal $status)
 
-    # reset cursor to block
-    switch $TERM
-        case 'xterm*'
-            printf '\033[2 q'
-        case 'rxvt*'
-            printf '\033[2 q'
-        case '*'
-            set -q TERM_PROGRAM; and [ $TERM_PROGRAM = "iTerm.app" ]; and printf '\033[2 q'
-    end
-
     printf '%s %s %s%s\n%s%s ' (__mrak_prompt_user) (__mrak_prompt_pwd) (__mrak_prompt_k8s) (__mrak_prompt_repo_info) (__mrak_prompt_env) (__mrak_prompt_symbol) | tr -s ' '
 end
