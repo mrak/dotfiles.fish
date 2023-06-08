f1901d65bfd4d1ff3304840e024e2b9753ba0858
Eric Mrak <enmrak@gmail.com>
2023-06-07 17:36:17 -0700

    env -C is not on macos

--- functions/,v.fish
+++ functions/,v.fish
@@ -1,4 +1,4 @@
 function ,v --description "Open init.vim for editing"
-    env -C $XDG_CONFIG_HOME/nvim $EDITOR $XDG_CONFIG_HOME/nvim/init.vim $argv
+    env $EDITOR $XDG_CONFIG_HOME/nvim/init.vim $argv
 end
 
