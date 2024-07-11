e062195f76f35b383aeae90675cc4475db68adc7
Eric Mrak <eric.mrak@fastly.com>
2024-07-11 11:43:49 -0700

    theme

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,7 +1,7 @@
 function fish_right_prompt
     set -l last_status $status
     [ $last_status = 0 ]; and return
-    set_color brblack
+    set_color $fish_color_status
     fish_status_to_signal $last_status
     set_color normal
 end
