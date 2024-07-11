3be675499ea1278e7f6ca3567815419a688be9de
Eric Mrak <eric.mrak@fastly.com>
2024-07-11 09:09:19 -0700

    root cwd color

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,9 +1,14 @@
 function fish_prompt
     set -l last_status $status
+    set -l color_cwd $fish_color_cwd
+
+    if fish_is_root_user
+        set $fish_color_cwd_root
+    end
 
     string join '' --                                    \
         (__mrak_prompt_user)                             \
-        ' ' (set_color green) (prompt_pwd)               \
+        ' ' (set_color $color_cwd) (prompt_pwd)          \
         (__mrak_prompt_k8s)                              \
         (fish_git_prompt | string replace -ra '[()]' '') \
 
