428ee2bc8fa6a2eee8f5041ecc01e54f89328339
Eric Mrak <enmrak@gmail.com>
2024-07-11 20:48:45 -0700

    repkg

--- conf.d/mrak.fish
+++ conf.d/mrak.fish
@@ -69,8 +69,6 @@ abbr --position command --add g git
 abbr --position command --add k kubectl
 abbr --position command --add kns kubens
 abbr --position command --add kctx kubectx
-abbr --position command --add p pacman
-abbr --position command --add a aura
 abbr --position command --add tf terraform
 abbr --position command --add v env \$VISUAL
 abbr --position command --add e env \$EDITOR
@@ -80,14 +78,18 @@ abbr --position command --add rmrf rm -rf
 abbr --position command --add cdd --set-cursor=% cd \$DEV_HOME/%
 abbr --position command --add cdp --set-cursor=% cd \$DEV_HOME/Projects/%
 abbr --position command --add cds --set-cursor=% cd \$DEV_HOME/Sources/%
-abbr --position command --add cdbuild --set-cursor=% cd \$DEV_HOME/PKGBUILDs/%
-abbr --position command --add cdaur --set-cursor=% cd \$DEV_HOME/AUR/%
-abbr --position command --add cdabs --set-cursor=% cd /var/abs/%
 abbr --position command --add glsg git ls-files \| grep
 abbr --position command --add xrdbmerge xrdb -quiet -merge \$HOME/.Xresources
 abbr --position command --add serve python3 -m http.server
 switch (uname)
     case Linux
+        abbr --position command --add cdbuild --set-cursor=% cd \$DEV_HOME/PKGBUILDs/%
+        abbr --position command --add cdaur --set-cursor=% cd \$DEV_HOME/AUR/%
+        abbr --position command --add cdabs --set-cursor=% cd /var/abs/%
+        abbr --position command --add p pacman
+        abbr --position command --add a aura
+        abbr --position command --add repkg makepkg -scrfi
+
         abbr --position command --add o xdg-open
         abbr --position command --add orphans sudo pacman -Rs \(pacman -Qqdt\)
         abbr --position command --add upgrade paccache -rk2\; and paccache -ruk0\; and sudo pacman -Syu
