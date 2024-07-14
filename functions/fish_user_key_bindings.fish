4f5fac929522f4112ddbe7fe859efc59a8c605a9
Eric Mrak <enmrak@gmail.com>
2024-07-13 21:11:49 -0700

    cleanup unused term settings

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -5,16 +5,9 @@ function fish_user_key_bindings
     type -q fzf_key_bindings; and fzf_key_bindings
     bind \e. history-token-search-backward
 
-    bind -k ppage history-search-backward
     switch $TERM
-        case dvtm-256color
-            bind \cH backward-kill-path-component
         case screen-256color st-256color
             bind \cH backward-kill-path-component
             bind \e\[M kill-word
-        case rxvt-unicode-256color
-            bind \cH backward-kill-path-component
-            bind \e\[3^ kill-word
-        case xterm-256color
     end
 end
