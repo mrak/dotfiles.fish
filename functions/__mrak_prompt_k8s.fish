ecef935cffbe19cc01100c873097a65171d8d414
Eric Mrak <eric.mrak@fastly.com>
2024-08-14 16:47:49 -0700

    fix spacing when no k8s context

--- functions/__mrak_prompt_k8s.fish
+++ functions/__mrak_prompt_k8s.fish
@@ -2,8 +2,7 @@ function __mrak_prompt_k8s --description "prompt info for kubernetes context"
     command -s kubectl >/dev/null; or return
     [ -f ~/.kube/config ]; or return
 
-    printf ' '
     set_color blue
-    kubectl config get-contexts | awk '/\*/ {printf "⬟ %s:%s", $2, ($5=="" ? "default" : $5)}'
+    kubectl config get-contexts | awk '/\*/ {printf " ⬟ %s:%s", $2, ($5=="" ? "default" : $5)}'
     set_color normal
 end
