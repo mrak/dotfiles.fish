9867d7a89a0a0e1f50e9369197e021e85d0efb34
Eric Mrak <eric.mrak@fastly.com>
2024-09-13 15:06:06 -0700

    fix pluginx

--- config.fish
+++ config.fish
@@ -1,17 +1,14 @@
 status is-interactive; or exit
 
 for plugin in (find $__fish_config_dir/plugins -type d -not -path plugins -depth 1)
-    if [ -d $plugin/completions; and not contains $plugin/completions $fish_complete_path ]
+    if [ -d $plugin/completions ]; and not contains $plugin/completions $fish_complete_path
         set fish_complete_path $plugin/completions $fish_complete_path
     end
-    if [ -d $plugin/functions; and not contains $plugin/functions $fish_complete_path ]
+    if [ -d $plugin/functions ]; and not contains $plugin/functions $fish_complete_path
         set fish_complete_path $plugin/functions $fish_complete_path
     end
     for conf in $plugin/conf.d/*.fish
         source $conf
-    end
-    if [ -d $plugin/completions; and not contains $plugin/completions $fish_complete_path ]
-        set fish_complete_path $plugin/completions $fish_complete_path
     end
 end
 
