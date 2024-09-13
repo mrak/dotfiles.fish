1bb615832714216ef51dd62cf4cb212d16180977
Eric Mrak <eric.mrak@fastly.com>
2024-09-13 15:16:48 -0700

    plugins fix again

--- conf.d/plugins.fish
+++ conf.d/plugins.fish
@@ -3,7 +3,7 @@ for plugin in (find $__fish_config_dir/plugins -type d -not -path plugins -depth
         set fish_complete_path $plugin/completions $fish_complete_path
     end
     if [ -d $plugin/functions ]; and not contains $plugin/functions $fish_function_path
-        set fish_function_path $plugin/functions $fish_complete_path
+        set fish_function_path $plugin/functions $fish_function_path
     end
     for conf in $plugin/conf.d/*.fish
         source $conf
