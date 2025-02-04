d44e6e54a83fdd77f93191bbe089b0bbc1f1b766
Eric Mrak <eric.mrak@fastly.com>
2025-02-04 10:18:22 -0800

    pythonprompt

--- functions/__mrak_prompt_env.fish
+++ functions/__mrak_prompt_env.fish
@@ -1,7 +1,7 @@
 function __mrak_prompt_env
     if set -q VIRTUAL_ENV
         set_color green
-        printf '(🐍  %s) ' (basename $VIRTUAL_ENV)
+        printf '🐍%s ' (basename $VIRTUAL_ENV)
         set_color normal
     end
 end
