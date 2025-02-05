c7a79c1ceb2c5649c394b51bd3ae452fa4a586c5
Eric Mrak <eric.mrak@fastly.com>
2025-02-05 11:20:35 -0800

    selectively setup cinc

---
+++ functions/cincup.fish
@@ -0,0 +1,10 @@
+if command -q cinc
+    function cincup --description "setup shell for cinc/chef use"
+        set -l init_cache $XDG_CACHE_HOME/.chef-shell-init.fish
+        find $init_cache -mtime +1d -delete > /dev/null 2>&1
+        if [ ! -f $init_cache ]
+            cinc shell-init fish > $init_cache
+        end
+        source $init_cache
+    end
+end
