e20d654b73dca28582468e3062fcb3edf1776dc3
Eric Mrak <enmrak@gmail.com>
2025-03-05 22:33:42 -0800

    fix false negatives for whoneeds

--- functions/whoneeds.fish
+++ functions/whoneeds.fish
@@ -2,7 +2,7 @@ if type -q pacman
     function whoneeds --description "List system packages depending on provided package"
         comm -12 (pactree -lru $argv[1]|sort|psub) \
             (pacman -Qqe|sort|psub) \
-            | grep -v $argv[1]
+            | grep -v "^$argv[1]\$"
     end
 else if type -q brew
     function whoneeds --description "List system packages depending on provided package"
