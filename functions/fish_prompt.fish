57ee0064fdd78981af002c3550d8242c43bb7b45
Eric Mrak <eric.mrak@fastly.com>
2025-03-17 11:38:41 -0700

    use old fish prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -6,11 +6,12 @@ function fish_prompt
         set $fish_color_cwd_root
     end
 
-    string join '' --                                    \
-        (__mrak_prompt_user)                             \
-        ' ' (set_color $color_cwd) (prompt_pwd)          \
-        (__mrak_prompt_k8s)                              \
-        (fish_git_prompt | string replace -ra '[()]' '') \
+    string join '' --                           \
+        (__mrak_prompt_user)                    \
+        ' ' (set_color $color_cwd) (prompt_pwd) \
+        (__mrak_prompt_k8s)                     \
+        ' '                                     \
+        (__mrak_prompt_repo_info)               \
 
     string join '' -- (__mrak_prompt_env) (__mrak_prompt_symbol $last_status) ' '
 end
