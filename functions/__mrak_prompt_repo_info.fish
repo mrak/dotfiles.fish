0a2a100113c376b33e6bf63a9f9817ce718a04e3
Eric Mrak <enmrak@gmail.com>
2025-03-17 21:37:39 -0700

    fix merge indicators

--- functions/__mrak_prompt_repo_info.fish
+++ functions/__mrak_prompt_repo_info.fish
@@ -1,91 +1,79 @@
 function __mrak_prompt_repo_info --description "prompt info for git"
-    set -l folder $PWD
+    command -s git >/dev/null; or return
 
+    set -l git_dir $PWD
     while true
-        if [ -r $folder/.git ]; and [ -d $folder/.git ]
-            __git_state $folder
+        if [ -r $git_dir/.git ]; and [ -d $git_dir/.git ]
             break
         end
-        set folder (path dirname $folder); or break
+        set git_dir (path dirname $git_dir); or return
     end
+    set -l git_dot_dir $git_dir/.git
 
-    set_color normal
-end
-
-function __git_ongoing --description "rebasing/merging/bisecting/etc"
-    set -l git_dot_dir $argv[1]
     set -l merging "∑"
     set -l rebasing "↨"
     set -l bisecting "%"
     set -l cherry "¤"
-
-    if [ -f $git_dot_dir/rebase-merge/head-name ]
-        printf '%s %s' (string replace -r '^.*/' '' < "$git_dot_dir/rebase-merge/head-name") $rebasing
-    else if [ -f $git_dot_dir/rebase-merge/interactive ]
-        printf %s $rebasing
-    else if [ -d $git_dot_dir/rebase-merge ]
-        printf %s $rebasing$merging
-    else if [ -d $git_dot_dir/rebase-apply ]
-        if [ -f $git_dot_dir/rebase-apply/rebasing ]
-            printf %s $rebasing
-        else if [ -f $git_dot_dir/rebase-apply/applying ]
-            printf %s AM
-        else
-            printf %s AM/REBASE
-        end
-    else if [ -f $git_dot_dir/MERGE_HEAD ]
-        printf %s $merging
-    else if [ -f $git_dot_dir/MERGE_MSG ]
-        printf %s $merging
-    else if [ -f $git_dot_dir/CHERRY_PICK_HEAD ]
-        printf %s $cherry
-    else if [ -f $git_dot_dir/BISECT_LOG ]
-        printf %s $bisecting
-    else
-        set -l tag (git -C $__git_dir --no-optional-locks tag --points-at HEAD) ∅
-        printf %s $tag[1]
-    end
-end
-
-function __git_state --description "git state for fish prompt"
-    command -s git >/dev/null; or return
-
-    set -l __git_dir $argv[1]
-    set -l git_dot_dir $__git_dir/.git
-    [ -z $git_dot_dir ]; and return
+    set -l nobranch "∅"
 
     set -l untracked "?" false
     set -l modified "±" false
     set -l deleted - false
     set -l staged "=" false
+    set -l unmerged "!" false
 
     set -l ahead "↑"
     set -l behind "↓"
     set -l diverged "↓↑"
 
-    set -l unmerged "!" false
-
+    set -l branch
+    set -l state
     set -l g ""
     if [ $UNAME = Darwin ]
-        set g (git -C $__git_dir --no-optional-locks status -uno --ignore-submodules=dirty --porcelain -b 2>/dev/null)
+        set g (git -C $git_dir --no-optional-locks status -uno --ignore-submodules=dirty --porcelain -b 2>/dev/null)
     else
-        set g (git -C $__git_dir --no-optional-locks status -uno --ignore-submodules=dirty --porcelain -b | uniq -w 2 2>/dev/null)
+        set g (git -C $git_dir --no-optional-locks status -uno --ignore-submodules=dirty --porcelain -b | uniq -w 2 2>/dev/null)
     end
 
-    set_color yellow
-    if string match -qr '^## HEAD' $g[1]
-        __git_ongoing $git_dot_dir
-    else
-        string replace -ra '(^## |\.\.\..*$)' '' $g[1]
-        switch $g[1]
-            case '* [ahead *, behind *]'
-                printf ' %s' $diverged
-            case '* [ahead *]'
-                printf ' %s' $ahead
-            case '* [behind *]'
-                printf ' %s' $behind
+    if [ -d $git_dot_dir/rebase-merge ]
+        set state $rebasing
+        if [ -f $git_dot_dir/rebase-merge/head-name ]
+            set branch (string replace -r '^.*/' '' < "$git_dot_dir/rebase-merge/head-name")
+        end
+    else if [ -d $git_dot_dir/rebase-apply ]
+        set state $rebasing
+        if [ -f $git_dot_dir/rebase-apply/rebasing ]
+            if [ -f $git_dot_dir/rebase-apply/head-name ]
+                set branch (string replace -r '^.*/' '' < "$git_dot_dir/rebase-merge/head-name")
+            end
+        end
+    else if [ -f $git_dot_dir/MERGE_HEAD ]
+        set state $merging
+    else if [ -f $git_dot_dir/CHERRY_PICK_HEAD ]
+        set state $cherry
+    else if [ -f $git_dot_dir/BISECT_LOG ]
+        set state $bisecting
+    end
+    if [ -z $branch ]
+        if string match -qr '^## HEAD' $g[1]
+            set -l tag (git -C $git_dir --no-optional-locks tag --points-at HEAD) $nobranch
+            set branch $tag[1]
+        else
+            set branch (string replace -ra '(^## |\.\.\..*$)' '' $g[1])
         end
     end
+
+    switch $g[1]
+        case '* [ahead *, behind *]'
+            set state $state $diverged
+        case '* [ahead *]'
+            set state $state $ahead
+        case '* [behind *]'
+            set state $state $behind
+    end
+
+    printf %s%s (set_color yellow) $branch
+    [ (count $state) -gt 0 ]; and printf ' %s' $state
 
     [ (count $g) -eq 1 ]; and return
     for l in $g[2..-1]
@@ -110,14 +98,15 @@ function __git_state --description "git state for fish prompt"
         end
     end
 
-    set_color brblack
     printf %s ' '
-    for state in staged modified deleted untracked
-        [ $$state[1][2] = true ]; and printf %s $$state[1][1]
-    end
     if [ $unmerged[2] = true ]
         set_color brred
         printf %s $unmerged[1]
     end
+    set_color brblack
+    for worktree in staged modified deleted untracked
+        [ $$worktree[1][2] = true ]; and printf %s $$worktree[1][1]
+    end
+
     set_color normal
 end
