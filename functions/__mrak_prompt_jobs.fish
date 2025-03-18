d4a35e532463830515bc51df6d26ce0944d2c065
Eric Mrak <eric.mrak@fastly.com>
2025-03-18 14:14:11 -0700

    fix accidental spacing

--- functions/__mrak_prompt_jobs.fish
+++ functions/__mrak_prompt_jobs.fish
@@ -2,7 +2,6 @@ function __mrak_prompt_jobs
     set -l job_count (count (jobs))
     if [ ! $job_count = 0 ]
         set_color brblack
-        printf '(%s)' $job_count
+        printf '(%s)%s' $job_count (set_color normal)
     end
-    set_color normal
 end
