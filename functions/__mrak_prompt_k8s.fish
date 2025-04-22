5c447dd7576929395e2cf858cc55cfda520f0ba5
Eric Mrak <eric.mrak@fastly.com>
2025-04-22 08:52:58 -0700

    minor speedup

--- functions/__mrak_prompt_k8s.fish
+++ functions/__mrak_prompt_k8s.fish
@@ -3,5 +3,5 @@ function __mrak_prompt_k8s --description "prompt info for kubernetes context"
     test -f ~/.kube/config; or return
 
     kubectl config get-contexts \
-    | awk '/\*/ {printf "'(set_color blue)'⬟ %s'(set_color brblack)'['(set_color blue)'%s'(set_color brblack)']", $2, ($5=="" ? "default" : $5)}'
+    | awk '/\*/ {printf "'(set_color blue)'⬟ %s'(set_color brblack)'['(set_color blue)'%s'(set_color brblack)']", $2, ($5=="" ? "default" : $5); exit}'
 end
