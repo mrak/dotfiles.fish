55060bb75806e0b0808316a127ec385b3713d438
Eric Mrak <enmrak@gmail.com>
2025-06-24 21:44:08 -0700

    path builtin in prompt env

--- functions/__mrak_prompt_env.fish
+++ functions/__mrak_prompt_env.fish
@@ -1,5 +1,5 @@
 function __mrak_prompt_env
     if set -q VIRTUAL_ENV
-        printf '%s🐍%s' (set_color green) (basename $VIRTUAL_ENV)
+        printf '%s🐍%s' (set_color green) (path basename $VIRTUAL_ENV)
     end
 end
