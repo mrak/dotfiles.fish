84ddf289efecdcee440164ef438dfc27d08c0d3c
Eric Mrak <enmrak@gmail.com>
2025-06-24 19:50:25 -0700

    date as right prompt

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,7 +1,9 @@
 function fish_right_prompt
     set -l last_status $status
-    test $last_status = 0; and return
     set_color $fish_color_status
-    fish_status_to_signal $last_status
+    # if test $last_status != 0
+    #     printf '(%s) ' (fish_status_to_signal $last_status)
+    # end
+    date +%T
     set_color normal
 end
