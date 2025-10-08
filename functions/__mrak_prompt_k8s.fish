fd7843fa12dd59cb96fc231490956acece79e0c3
Eric Mrak <eric.mrak@fastly.com>
2025-10-08 16:01:37 -0700

    terraform prompt, colored k8s for production

--- functions/__mrak_prompt_k8s.fish
+++ functions/__mrak_prompt_k8s.fish
@@ -1,7 +1,12 @@
 function __mrak_prompt_k8s --description "prompt info for kubernetes context"
     command -s kubectl >/dev/null; or return
     test -f ~/.kube/config; or return
+    set -l ctx_ns (kubectl config get-contexts | awk '/\*/ {printf "%s\n%s", $2, ($5=="" ? "default" : $5); exit}')
+    set -l color (set_color blue)
 
-    kubectl config get-contexts \
-    | awk '/\*/ {printf "'(set_color blue)'%s'(set_color brblack)'['(set_color blue)'%s'(set_color brblack)']", $2, ($5=="" ? "default" : $5); exit}'
+    if string match -qr 'plat|prd' $ctx_ns[1]
+        set color (set_color red)
+    end
+
+    printf "%s%s"(set_color brblack)"[%s%s"(set_color brblack)"]" $color $ctx_ns[1] $color $ctx_ns[2]
 end
